6e1a56626548566d71e19bc17692d7e4e5a2105e
SAUMYA PANDEY <saumya.pandey6211@gmail.com>
2026-08-01 19:33:26 +0530

    Time: 0 ms (100%), Space: 9.7 MB (69.71%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        std::vector<int> dp = nums; 
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                dp[j] = std::max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
+        }
+
+        return dp[n - 1] >= 0;
+    }
+};
